31a42356998a85d488edbae371628b29afec9977
Stephane Demotte <stephane@demotte.net>
2020-07-27 08:17:47 -0400

    big bang

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+// import HelloWorld from './components/HelloWorld.vue'
+
+const AsyncComp = path => defineAsyncComponent(() =>
+  import(`./components/${path}.vue`)
+)
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld: AsyncComp('HelloWorld')
+    // HelloWorld
+  }
+}
+</script>
